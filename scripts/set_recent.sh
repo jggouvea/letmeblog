d0fa7c8cf08cfac4cc00a7e78bd5acd28b6804a1
José Geraldo Gouvêa <jggouvea@yahoo.com>
2021-05-29 00:33:39 -0300

    several small improvements

--- scripts/set_recent.sh
+++ scripts/set_recent.sh
@@ -2,20 +2,20 @@
 
 source $(pwd | cut -d'/' -f1,2,3,4)/site.cfg
 
-rm -f recent.txt 
+rm -f recent.txt
 
 for recent in $(find src/posts -type f -iname \*.md | sort -ur | head -n 6)
-do 
+do
 entry="$(basename $recent .md).html"
 title=$(grep "title: " $recent | cut -d'"' -f2)
 plink="posts/$entry"
-teaser=$(cat $recent | sed -e '1,/^$/d' | sed '/^$/,$d' | pandoc ) 
+teaser=$(cat $recent | sed -e '1,/^$/d' | sed '/^$/,$d' | pandoc )
 cover=$(grep "coverImage:" $recent | cut -d'"' -f2)
-date=$(grep "date: " $recent | cut -d' ' -f2 | cut -d'"' -f2)
-pdate=$(date -u --date=$date '+%d/%m/%Y')
+date=`grep "^date: " $recent | cut -d' ' -f2 | cut -d'"' -f2`
+pdate=`date --date $date '+%d/%m/%Y'`
 
-if [ ! -z "$cover" ]; 
-then 
+if [ ! -z "$cover" ];
+then
 coverpic="<figure class=\"thumb\"><img src=\"/images/$cover\" alt=\"post thumbnail\" /></figure>"
 else
 coverpic=""
